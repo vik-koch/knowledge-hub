7493076c9b6d6a8ee528f4e5678affb198433397
viktor.koch <viktor.koch@xitaso.com>
2022-11-10 21:56:01 +0100

    Added custom InvalidConfigurationException

---
+++ knowledge-hub/src/main/java/com/khub/exceptions/InvalidConfigurationException.java
@@ -0,0 +1,10 @@
+package com.khub.exceptions;
+
+// Custom Exception for handling invalid configurations
+public class InvalidConfigurationException extends Exception {
+
+    public InvalidConfigurationException(String message) {
+        super(message);
+    }
+
+}
