d6d076fe3bae8976cc845daa18ce595395d25295
viktor.koch <viktor.koch@xitaso.com>
2022-12-02 09:54:11 +0100

    Added crawler runner for writing data to MongoDB

---
+++ knowledge-hub/src/main/java/com/khub/database/MongoConnector.java
@@ -0,0 +1,26 @@
+package com.khub.database;
+
+import java.util.Properties;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+// Wrapper class for MongoClient
+public class MongoConnector {
+
+    private static final String URL_KEY = "mongo.url";
+
+    private MongoConnector() {
+    }
+
+    /**
+     * Returns a {@code MongoClient} instance created from the parsed {@code Properties}
+     * @param configuration - {@code Properties} object
+     * @return a {@code MongoClient} instance
+     */
+    public static MongoClient getClient(Properties configuration) {
+        String url = configuration.getProperty(URL_KEY);
+        System.out.println(url);
+        return MongoClients.create(url);
+    }
+}
