f66051700d9e3e2ec63921d48abaf150d9bcae30
viktor.koch <viktor.koch@xitaso.com>
2023-01-16 08:35:25 +0100

    Added TDBHandler for Apache Jena TDB

---
+++ knowledge-hub/src/main/java/com/khub/jena/TDBHandler.java
@@ -0,0 +1,32 @@
+package com.khub.jena;
+
+import java.util.Properties;
+
+import org.apache.jena.query.Dataset;
+import org.apache.jena.tdb.TDBFactory;
+
+import com.khub.exceptions.InvalidConfigurationException;
+
+// Wrapper class for TDBFactory
+public class TDBHandler {
+
+    private static final String PATH_KEY = "tdb.path";
+
+    // Prevents instantiation
+    private TDBHandler() {
+    }
+
+    /**
+     * 
+     * @param configuration
+     * @return
+     * @throws InvalidConfigurationException
+     */
+    public static Dataset getDataset(Properties configuration) throws InvalidConfigurationException {
+        String path = configuration.getProperty(PATH_KEY);
+        if (path == null || path.isEmpty()) {
+            throw new InvalidConfigurationException("The given URL is null or empty");
+        }
+        return TDBFactory.createDataset(path);
+    }
+}
