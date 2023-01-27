7d1d9439de42783406c122072b7567ebe99dca3d
viktor.koch <viktor.koch@xitaso.com>
2023-01-27 09:34:20 +0100

    Added KnowledgeGraphEnricher

--- knowledge-hub/src/main/java/com/khub/mapping/ContentMappingRunner.java
+++ knowledge-hub/src/main/java/com/khub/mapping/ContentMappingRunner.java
@@ -91,8 +91,8 @@ public class ContentMappingRunner {
                         }
                     }
                 }
+                tdb.end();
             }
-            tdb.end();
 
         } catch (InvalidConfigurationException e) {
             logger.log(Level.SEVERE, "Invalid configuration provided", e);
