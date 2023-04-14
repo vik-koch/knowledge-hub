852bce0e0843bebb1846507841aa73930f2645da
Viktor Koch <viktor.koch@xitaso.com>
2023-04-14 15:23:33 +0200

    Removed unused import

--- khub-explorer/src/utilities/Voting.js
+++ khub-explorer/src/utilities/Voting.js
@@ -1,5 +1,4 @@
 import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Stack from 'react-bootstrap/Stack';
 
 function Voting({ size, voted, setVote }) {
@@ -7,7 +6,6 @@ function Voting({ size, voted, setVote }) {
   const disabled = voted || size === undefined || size === 0 || isNaN(size);
   console.log(size);
   return (
-    
     <Stack direction='horizontal' gap={3}>
         <Button disabled={disabled} variant='secondary' onClick={() => setVote('left')}>{'<<<'}</Button>
         <div>Which results are better?</div>
